80ca9d8de15930a9e92e52b93682c58fc7b07d1b
wendel <wendeldaniel263@gmail.com>
2022-06-27 21:49:29 -0300

    exercicio7 ok?

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(macaco, elefante) {
-  if (macaco && elefante === true){
+  if (macaco && elefante === true){ 
     return true
   } else {
     return false;
@@ -30,17 +30,40 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numeros) {
-  
-}
+  // let maiorNumero;
+  let contador = 0;
+  // for (let i = 0; i < numeros.length; i++) {
+  //   maiorNumero = numeros[i];
+  //   for (let j = 0; j < numeros.length; j++) {
+  //      if (maiorNumero > numeros[j]){
+  //         contador ++;
+  //      }
+  //   }
+  // }
+  // return contador;
 
+
+}
+console.log(highestCount([1,1,9]));
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(mouse  - cat1  < mouse  - cat2 ){
+    return ("cat2")
+  }if(mouse  - cat1  > mouse  - cat2){
+    return ("cat1")
+  }else{
+    return ('os gatos trombam e o rato foge')
+  }
 }
-
+  console.log(catAndMouse(1, 2, 2));
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let parametro = [];
+  for (const parametro of array) {
+    if (array % 3 === 0) {
+      
+    }
+  }
 }
 
 // Desafio 9
